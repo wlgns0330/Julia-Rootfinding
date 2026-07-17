172f154b3f0bc45fe6bd7a5613c1796cc376cb9c
JarvisRAs <ras4jarvis@gmail.edu>
2026-07-17 10:48:18 -0600

    Fix broken jupyter notebook kernel

--- src/YRoots.jl
+++ src/YRoots.jl
@@ -1,9 +1,7 @@
 module YRoots #This is the main module file, for versioning and test suite compatibility
 
-# Include other source files
-include("ChebyshevApproximator.jl")
+include("CombinedSolver.jl")
 
-# Optionally, re-export symbols from included files
-using .ChebyshevApproximator
+export solve
 
 end # module YRoots
